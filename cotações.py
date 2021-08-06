18e56249cb98cd86d92fe50c1815324736bd8127
Carlos Marinho <81977958+carlosmarinho8@users.noreply.github.com>
2021-08-06 15:30:50 -0300

    Update cotações.py

--- cotações.py
+++ cotações.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 import time
-
+# atualização c das cotas com loop infinito while
 while True:
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
     requisicao_dic = requisicao.json()
@@ -22,3 +22,25 @@ while True:
     print(f"Cotação Atualizada. {datetime.now()}")
     print('\n')
     time.sleep(60)
+    # atualização das cotas com limite de vezes com loop for
+    """
+    while True:
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
+    cotacao_euro = requisicao_dic["EURBRL"]["bid"]
+    cotacao_btc = requisicao_dic["BTCBRL"]["bid"]
+
+    tabela = pd.read_excel("Cotações.xlsx")
+    tabela.loc[0, "Cotação"] = float(cotacao_dolar)
+    tabela.loc[1, "Cotação"] = float(cotacao_euro)
+    tabela.loc[2, "Cotação"] = float(cotacao_btc) * 1000
+    tabela.loc[0, "Data Última Atualização"] = datetime.now()
+
+    tabela.to_excel("Cotações.xlsx", index=False)
+    print(tabela)
+    print(f"Cotação Atualizada. {datetime.now()}")
+    print('\n')
+    time.sleep(60)
+    """
